df4aedca326b5b1141ee7da52cd9c39515fcdec5
navaneeth arya <navaneetharyarao@gmail.com>
2025-05-04 21:21:29 +0530

    d

---
+++ src/main/java/program2/maven/program2_example_jar/App.java
@@ -0,0 +1,13 @@
+package program2.maven.program2_example_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Helloo World!" );
+    }
+}
